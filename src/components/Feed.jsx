32ae88f145d77243a11c5682744ee0cc6d0be28b
Nicholas Wease <nicholaswease@Nicholass-MacBook-Air.local>
2021-06-14 10:44:26 +0200

    Setup the Feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -3,17 +3,58 @@ import Product from './Product';
 
 function Feed({products}) {
     return (
-        <div>
+        <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto'>
             {
-                products.map(({id, title, description, category, image}) => (
+                products
+                    .slice(0, 4)
+                    .map(({id, title, description, category, image, price}) => (
                     <Product
                         key={id}
                         title={title}
                         description={description}
                         category={category}
                         image={image}
+                        price={price}
                     />
                 ))
+            }
+
+            <img
+                className='md:col-span-4'
+                src='https://links.papareact.com/dyz'
+                alt='Big'
+            />
+
+            <div className='md:col-span-2'>
+                {
+                    products
+                        .slice(4, 5)
+                        .map(({id, title, description, category, image, price}) => (
+                            <Product
+                                key={id}
+                                title={title}
+                                description={description}
+                                category={category}
+                                image={image}
+                                price={price}
+                            />
+                        ))
+                }
+            </div>
+
+            {
+                products
+                    .slice(5, products.length)
+                    .map(({id, title, description, category, image, price}) => (
+                        <Product
+                            key={id}
+                            title={title}
+                            description={description}
+                            category={category}
+                            image={image}
+                            price={price}
+                        />
+                    ))
             }
         </div>
     );
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,14 +1,22 @@
 import React, {useState} from 'react';
 import Image from 'next/image'
 import {StarIcon} from '@heroicons/react/solid';
+import Currency from 'react-currency-formatter';
 
-function Product({id, title, description, category, image}) {
+const MAX_RATING = 5;
+const MIN_RATING = 1;
 
-    const [rating] = useState(2);
+function Product({id, title, price, description, category, image}) {
+
+    const [rating] = useState(
+        Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
+    );
+
+    const [hasPrime] = useState(Math.random() < 0.5);
 
     return (
-        <div>
-            <p>
+        <div className='relative flex flex-col m-5 bg-white z-30  p-10'>
+            <p className='absolute top-2 right-2 text-xs italic text-gray-400'>
                 {category}
             </p>
 
@@ -20,27 +28,53 @@ function Product({id, title, description, category, image}) {
                 objectFit='contain'
             />
 
-            <h4>
+            <h4 className='my-3'>
                 {title}
             </h4>
 
             <div className='flex'>
                 {
                     Array(rating)
-                        .fill()
-                        .map((_, i) => (
-                            <StarIcon className='h-5' />
+                    .fill()
+                    .map((_, i) => (
+                        <StarIcon
+                            key={i}
+                            className='h-5 text-yellow-500'
+                        />
                     ))
                 }
             </div>
 
-            <p>
-
+            <p className='text-xs my-2 line-clamp-2'>
+                {description}
             </p>
 
-            <p>
+            <div className='mb-5'>
+                <Currency
+                    quantity={price}
+                    currency='USD'
+                />
+            </div>
 
-            </p>
+            {
+                hasPrime && (
+                    <div className='flex items-center space-x-2 -mt-5'>
+                        <img
+                            className='w-12'
+                            src='https://links.papareact.com/fdw'
+                            alt='Prime'
+                        />
+
+                        <p className='text-xs text-gray-500'>
+                            FREE Next-day Delivery
+                        </p>
+                    </div>
+                )
+            }
+
+            <button className='mt-auto button'>
+                Add to Basket
+            </button>
         </div>
     );
 }
